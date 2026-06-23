1dc1fea3884774b8479dfcd3fe0df6d61534a824
Santiago M <fsmm19@proton.me>
2026-06-23 17:49:32 -0500

    add login and user controllers

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { MONGODB_URI } from './utils/config.js';
 import { logInfo, logError } from './utils/logger.js';
 import notesRouter from './controllers/notes.js';
 import usersRouter from './controllers/users.js';
+import loginRouter from './controllers/login.js';
 import {
   requestLogger,
   unknownEndpoint,
@@ -27,6 +28,7 @@ app.use(requestLogger);
 
 app.use('/api/notes', notesRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
